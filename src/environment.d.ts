68bbf3e02cf45a6cc655c56c9cf4a565b1257f2c
Kelton <kadey001@ucr.edu>
2020-07-23 12:39:42 -0700

    Added rabbitmq & updated env

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -5,16 +5,12 @@ declare global {
             PORT?: string;
             ORIGIN?: string;
             DB_URL?: string;
+            AMQP_URL?: string;
             JWT_SECRET?: string;
             MAILGUN_ORIGIN?: string;
             MAILGUN_API_KEY?: string;
             MAILGUN_DOMAIN?: string;
             MAILGUN_FROM_EMAIL?: string;
-            GMAIL_USER?: string;
-            GMAIL_ID?: string;
-            GMAIL_SECRET?: string;
-            GMAIL_REFRESH_TOKEN?: string;
-            GMAIL_ACCESS_TOKEN?: string;
         }
     }
 }
