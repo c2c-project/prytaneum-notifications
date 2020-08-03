db07d0888fa6c4cbf087092dd381da252e014e63
Kelton <kadey001@ucr.edu>
2020-08-03 12:22:36 -0700

    Updated email to use batching and recipient vars

--- src/lib/emails/email.ts
+++ src/lib/emails/email.ts
@@ -1,7 +1,11 @@
 import mailgun from 'mailgun-js';
 import env from '../../config/env';
+// import logger from '../logger';
 
-const mg = mailgun({ apiKey: env.MAILGUN_API_KEY, domain: env.MAILGUN_DOMAIN });
+const mg = mailgun({
+    apiKey: env.MAILGUN_API_KEY,
+    domain: env.MAILGUN_DOMAIN,
+});
 
 /**
  * @description internal function to use mg api to send email
@@ -9,39 +13,41 @@ const mg = mailgun({ apiKey: env.MAILGUN_API_KEY, domain: env.MAILGUN_DOMAIN });
  * @param {string} subject subject of the email
  * @param {string} text body of the email
  * @param {Date} deliveryTime Time that the email should be sent out (If the date is in the past it will be sent immediately)
- * @returns {Promise<any>}
+ * @returns {Promise<string | mailgun.messages.SendResponse>}
  */
 const sendEmail = async (
-    to: string,
+    to: string | Array<string>,
     subject: string,
     text: string,
-    deliveryTime: Date
-): Promise<any> => {
+    deliveryTime: Date,
+    recipientVariables: string
+    // eslint-disable-next-line consistent-return
+): Promise<string | mailgun.messages.SendResponse> => {
     // in development mode, don't send an email, instead we will test this on the staging server
-    console.log(to);
-    if (env.NODE_ENV === 'development') {
-        const data: mailgun.messages.SendData = {
+    // TODO allow user to pass through custom api key and domain
+    if (env.NODE_ENV === 'test') {
+        return new Promise<string>((resolve) => resolve('success'));
+        return mg.messages().send({
             to,
             from: `Prytaneum <${env.MAILGUN_FROM_EMAIL}>`,
             subject,
             text,
+            'recipient-variables': recipientVariables,
             'o:deliverytime': deliveryTime.toUTCString(),
             'o:testmode': 'true',
-        };
-        return await mg.messages().send(data);
-    } else if (env.NODE_ENV === 'test') {
-        console.log(`To: ${to}: \n${text}`);
-        return new Promise((resolve) => resolve('success'));
-    } else {
-        const data: mailgun.messages.SendData = {
-            to,
-            from: `Prytaneum <${env.MAILGUN_FROM_EMAIL}>`,
-            subject,
-            text,
-            'o:deliverytime': deliveryTime.toUTCString(),
-        };
-        return await mg.messages().send(data);
+        });
     }
+    if (env.NODE_ENV === 'development') {
+        return new Promise<string>((resolve) => resolve('success'));
+    }
+    return mg.messages().send({
+        to,
+        from: `Prytaneum <${env.MAILGUN_FROM_EMAIL}>`,
+        subject,
+        text,
+        'recipient-variables': recipientVariables,
+        'o:deliverytime': deliveryTime.toUTCString(),
+    });
 };
 
 export default {
