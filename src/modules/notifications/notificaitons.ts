381a7bd5d6fac7bd8820f4c7325c805630001c9f
Kelton <kadey001@ucr.edu>
2020-08-03 12:46:29 -0700

    Updated notifications module

--- src/modules/notifications/notificaitons.ts
+++ src/modules/notifications/notificaitons.ts
@@ -1,31 +1,38 @@
-import Email from '../../lib/emails/email';
+/* eslint-disable @typescript-eslint/indent */
+import Mailgun from 'mailgun-js';
+import Email from '../../lib/emails';
 
 /**
  * @description Send out email notificaions from the subList
  * @param {Array<string>} subList A list of subscribers
  * @param {Date} deliveryTime Time that the email will be delivered
  */
-const notifyMany = async (subList: Array<string>, deliveryTime: Date) => {
-    try {
-        if (subList.length === 0) {
-            throw new Error('Empty subscribe list');
-        }
-        let results: Array<string> = [];
-        //TODO Fix the async problem
-        for (let i = 0; i < subList.length; i++) {
-            const email = subList[i];
-            const result = await Email.sendEmail(
-                email,
-                'Prytaneum Notification',
-                'Test Notification',
-                deliveryTime
-            );
-            results.push(result);
-        }
-        return results;
-    } catch (e) {
-        console.error(e);
+const notifyMany = async (
+    subList: Array<string>,
+    deliveryTime: Date
+): Promise<Array<string | Mailgun.messages.SendResponse>> => {
+    if (subList.length === 0) {
+        throw new Error('Empty subscribe list');
     }
+    const results: Array<Promise<string | Mailgun.messages.SendResponse>> = [];
+    while (subList.length) {
+        // Take max of 1k emails to send for each mailgun batch
+        const emails = subList.slice(0, 1000);
+        // TODO Figure out what should be sent for notification
+        const recipientVariables = '';
+        const notificationString = 'Test Notification';
+        // eslint-disable-next-line no-await-in-loop
+        results.push(
+            Email.sendEmail(
+                emails,
+                'Prytaneum Notification',
+                notificationString,
+                deliveryTime,
+                recipientVariables
+            )
+        );
+    }
+    return Promise.all(results);
 };
 
 export default { notifyMany };
--- src/modules/notifications/notifications.test.ts
+++ src/modules/notifications/notifications.test.ts
@@ -7,18 +7,13 @@ describe('Notifications Module', () => {
     describe('notifyMany', () => {
         it('should accept valid data', async () => {
             const results = await Notify.notifyMany(subList, deliveryTime);
-            console.log('RESULTS:', results);
             expect(results).toBeDefined();
             expect(results).toStrictEqual(['success']);
         });
         it('should throw an error with empty list', async () => {
-            try {
-                const results = await Notify.notifyMany([], deliveryTime);
-                expect(results).toBeUndefined();
-            } catch (e) {
-                expect(e).toBeInstanceOf(Error);
-                expect(e.message).toStrictEqual('Empty subscribe list');
-            }
+            await expect(
+                Notify.notifyMany([], deliveryTime)
+            ).rejects.toThrowError();
         });
     });
 });
