5c82e2fe8a938a39a760eca0d654cefd9f947628
Kelton <kadey001@ucr.edu>
2020-09-24 14:22:32 -0700

    Removed additional Rabbit MQ references

--- src/lib/jobs/notifications.ts
+++ src/lib/jobs/notifications.ts
@@ -1,50 +1,45 @@
 /* eslint-disable @typescript-eslint/indent */
 import Mailgun from 'mailgun-js';
-import Rabbitmq from '../rabbitmq';
 import DB from '../notifications';
 import Notifications from '../../modules/notifications';
 import logger from '../logger';
 
 export interface notifiationConsumerData {
     region: string;
-    notificationDateISO: string;
+    notificationDate: Date;
 }
 
-const delay = (duration: number) =>
-    new Promise((resolve) => setTimeout(resolve, duration));
-
 /**
- * @description Checks the rabbitmq notification queue and queues up the pending notifications.
+ * @description Checks the notification queue and queues up the pending notifications.
  * @returns {Promise<void>}
  */
-const notificationConsumer = async (): Promise<void> => {
-    try {
-        logger.print('Running Notification Consumer');
-        const queue = 'notifications';
-        const channel = Rabbitmq.getChannel();
-        await channel.assertQueue(queue);
-        const notifications: Array<notifiationConsumerData> = [];
-        await channel.consume(queue, (msg) => {
-            if (msg) {
-                notifications.push(JSON.parse(msg.content.toString()));
-                channel.ack(msg);
-            }
-        });
-        const results: Array<Promise<
-            Array<string | Mailgun.messages.SendResponse>
-        >> = [];
-        // For each of the notification jobs, check relevant subscribers and send out notifcations to each
-        for (let i = 0; i < notifications.length; i += 1) {
-            const { region, notificationDateISO } = notifications[i];
-            // eslint-disable-next-line no-await-in-loop
-            const subList = await DB.getSubList(region);
-            const date = new Date(notificationDateISO);
-            results.push(Notifications.notifyMany(subList, date));
-        }
-        await Promise.all(results);
-    } catch (e) {
-        logger.err(e);
+const notificationConsumer = (): void => {
+    logger.info('Running Notification Consumer');
+    // TODO Get notifications from database (query & consume notifications within time frame)
+    const notifications: Array<notifiationConsumerData> = [];
+    const notificationResults: Array<Promise<
+        Array<string | Mailgun.messages.SendResponse>
+    >> = [];
+    // For each of the notification jobs, check relevant subscribers and send out notifcations to each
+    for (let i = 0; i < notifications.length; i += 1) {
+        const { region, notificationDate } = notifications[i];
+        DB.getSubList(region)
+            .then((subList) => {
+                notificationResults.push(
+                    Notifications.notifyMany(subList, notificationDate)
+                );
+            })
+            .catch((e) => {
+                logger.error(e);
+            });
     }
+    Promise.all(notificationResults)
+        .then((results) => {
+            logger.info(`Notification Results: ${JSON.stringify(results)}`);
+        })
+        .catch((e) => {
+            logger.error(e);
+        });
 };
 
 export default notificationConsumer;
