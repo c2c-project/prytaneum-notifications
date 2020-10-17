576175e5bfaebc1580e3280ef937c36594fcd31a
Kelton <kadey001@ucr.edu>
2020-10-16 17:10:07 -0700

    Updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,17 @@
+var path = require('path');
+
 module.exports = {
     collectCoverage: true,
+    rootDir: path.resolve(__dirname, './'),
     coverageDirectory: './coverage',
-    coverageReporters: ['text'],
+    coverageReporters: ['text', 'json', ['lcov', { projectRoot: './' }]],
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0,
+        },
+    },
+    moduleDirectories: ['node_modules', 'src'],
 };
